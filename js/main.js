1b3f4af8373b9eb45b50ed32bc0a3996a603cbd0
Irene García <irenegwodak@gmail.com>
2023-09-08 11:30:47 +0200

    add conditionals with description search

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-"use-strict";
-const jsList = document.querySelector(".js-list");
-
+'use-strict';
+const jsList = document.querySelector('.js-list');
 
 const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
 const kittenName1 = 'Anastasio';
-const kittenDesc1 = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-const kittenRace1 = 'Siamés'
+const kittenDesc1 =
+  'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenRace1 = 'Siamés';
 
 const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp';
 const kittenName2 = 'Fiona';
-const kittenDesc2 = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.'; 
+const kittenDesc2 =
+  'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
 const kittenRace2 = 'Sphynx';
 
 const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
 const kittenName3 = 'Cielo';
-const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.'
-const kittenRace3 = 'Maine Coon'; 
+const kittenDesc3 =
+  'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const kittenRace3 = 'Maine Coon';
 
 const kittenOne = `<li class="card">
 <article>
@@ -27,7 +29,7 @@ const kittenOne = `<li class="card">
   <h3 class="card_title">${kittenName1}</h3>
   <h4 class="card_race">${kittenRace1}</h4>
   <p class="card_description">
-            ${kittenDesc1}
+  ${kittenDesc1}
    </p>
 </article>
 </li>`;
@@ -45,7 +47,7 @@ ${kittenDesc2}
   </p>
   </li>`;
 
-  const kittenThree = `<li class="card">
+const kittenThree = `<li class="card">
   <img
   class="card_img"
   src="${kittenImage3}"
@@ -58,4 +60,21 @@ ${kittenDesc2}
   </p>
   </li>`;
 
-jsList.innerHTML = kittenOne + kittenTwo + kittenThree;
+// jsList.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
+
+let html = '';
+
+if( kittenDesc1.includes(descrSearchText) ) {
+  jsList.innerHTML = kittenOne;
+  }
+  
+if( kittenDesc2.includes(descrSearchText) ) {
+  jsList.innerHTML = jsList.innerHTML + kittenTwo;
+  }
+  
+if( kittenDesc3.includes(descrSearchText) ) {
+  jsList.innerHTML = jsList.innerHTML + kittenThree;
+  }
