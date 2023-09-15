d8cc8e847a3760c488104af1e165d8882e4c3fab
ArantzaGG <arantzagomez@hotmail.es>
2023-09-15 11:29:06 +0200

    array kittendata

--- js/main.js
+++ js/main.js
@@ -17,83 +17,54 @@ const jsNewformAdd = document.querySelector('.js-newform-add');
 const jsNewformCancel = document.querySelector('.js-newform-cancel');
 const jsBtnSearch = document.querySelector('.js_btn_search');
 
-
 //datos generales
 
 const descrSearchText = input_search_desc.value;
 
-const kittenData_1 = {
-  image: 'https://dev.adalab.es/gato-siames.webp',
-  name: 'Anastacio',
-  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
-  race: 'Siamés'
-};
-const kittenData_2 = {
-  image : 'https://dev.adalab.es/sphynx-gato.webp',
-  name : 'Fiona',
-  desc : 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
-  race : 'Sphynx'
-};
-const kittenData_3 = {
-   image : 'https://dev.adalab.es/maine-coon-cat.webp',
-   name : 'Cielo',
-   desc : 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
-   race : 'Maine Coon'
-  };
-
+const kittenDataList = [
+  {
+    image: 'https://dev.adalab.es/gato-siames.webp',
+    name: 'Anastacio',
+    desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+    race: 'Siamés',
+  },
+  {
+    image: 'https://dev.adalab.es/sphynx-gato.webp',
+    name: 'Fiona',
+    desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+    race: 'Sphynx',
+  },
+  {
+    image: 'https://dev.adalab.es/maine-coon-cat.webp',
+    name: 'Cielo',
+    desc: 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+    race: 'Maine Coon',
+  },
+];
 
 //funciones
 
-function renderKitten(kittenData_1) {
+function renderKitten(kittenData) {
   return `<li class="card">
 <article>
   <img
     class="card_img"
-    src= "${kittenData_1.image}"
+    src= "${kittenData.image}"
     alt="gatito"
   />
-  <h3 class="card_title">${kittenData_1.name}</h3>
-  <h4 class="card_race">${kittenData_1.race}</h4>
+  <h3 class="card_title">${kittenData.name}</h3>
+  <h4 class="card_race">${kittenData.race}</h4>
   <p class="card_description">
-  ${kittenData_1.desc}
+  ${kittenData.desc}
    </p>
 </article>
 </li>`;
 }
 
-function renderKitten(kittenData_2) {
-  return `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src= "${kittenData_2.image}"
-    alt="gatito"
-  />
-  <h3 class="card_title">${kittenData_2.name}</h3>
-  <h4 class="card_race">${kittenData_2.race}</h4>
-  <p class="card_description">
-  ${kittenData_2.desc}
-   </p>
-</article>
-</li>`;
-}
-
-function renderKitten(kittenData_3) {
-  return `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src= "${kittenData_3.image}"
-    alt="gatito"
-  />
-  <h3 class="card_title">${kittenData_3.name}</h3>
-  <h4 class="card_race">${kittenData_3.race}</h4>
-  <p class="card_description">
-  ${kittenData_3.desc}
-   </p>
-</article>
-</li>`;
-}
+jsList.innerHTML =
+  renderKitten(kittenDataList[0]) +
+  renderKitten(kittenDataList[1]) +
+  renderKitten(kittenDataList[2]);
 
 function showNewCatForm() {
   newForm.classList.remove('collapsed');
@@ -117,7 +88,7 @@ function addNewKitten(event) {
     image: inputPhoto.value,
     name: inputName.value,
     desc: inputDesc.value,
-    race: inputRace.value
+    race: inputRace.value,
   };
   if (
     newKitten.desc === '' ||
@@ -143,14 +114,14 @@ jsNewformCancel.addEventListener('click', () => {
 
 const filterKitten = (event) => {
   event.preventDefault();
-  if (kittenData_1.desc.includes(descrSearchText)) {
-    jsList.innerHTML += renderKitten(kittenData_1);
+  if (kittenDataList[0].desc.includes(descrSearchText)) {
+    jsList.innerHTML += renderKitten(kittenDataList[0]);
   }
-  if (kittenData_2.desc.includes(descrSearchText)) {
-    jsList.innerHTML += renderKitten(kittenData_2);
+  if (kittenDataList[1].desc.includes(descrSearchText)) {
+    jsList.innerHTML += renderKitten(kittenDataList[1]);
   }
-  if (kittenData_3.desc.includes(descrSearchText)) {
-    jsList.innerHTML += renderKitten(kittenData_3);
+  if (kittenDataList[2].desc.includes(descrSearchText)) {
+    jsList.innerHTML += renderKitten(kittenDataList[2]);
   }
 };
 
